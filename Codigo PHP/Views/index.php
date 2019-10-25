5478cd17559ec31befe627682dc1f89e8ff25281
flavio <favio789@gmail.com>
2019-10-24 21:32:30 -0500

    se añadio fondo a index

---
+++ Codigo PHP/Views/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+
+<link rel="stylesheet" type = "text/css" href="../css/styles.css">
+<title>Tienda</title>
+</head>
+<body>
+<h1 id="T_principal">Ventas</h1>
+</body>
+</html>
