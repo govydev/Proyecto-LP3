96d4a66f42d2736b598e46d1799d8775a31fa794
flavio <favio789@gmail.com>
2019-10-24 19:17:22 -0500

    creacion de proyecto
    division en carpetas
    se agrego parametros y funcion de conexion a BD

---
+++ Codigo PHP/Conexion/conexion.php
@@ -0,0 +1,17 @@
+<?php
+ include("config.php");
+
+class Conexion{
+    protected $conexion_db;
+    //contructor
+	public function Conexion(){
+        //el uso de (mysqli) es necesario a partir de versiones superiores a PHP 5.4
+        $this->conexion_db = new mysqli(DB_HOST, DB_USUARIO, DB_CONTRA, DB_NOMBRE);
+
+        if($this->conexion_db->connect_errno){
+            echo "Fallo al conectar a Mysql: ".$this->conexion_db->connect_error;
+            return;
+        }	
+        $this->conexion_db->set_charset(DB_CHARSET);
+    }	
+}
