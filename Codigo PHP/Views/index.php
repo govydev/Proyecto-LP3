ec5cfb899a7a51166eb6cc74b104d13495d7e376
flavio <favio789@gmail.com>
2019-10-25 18:21:24 -0500

    se elimino bootstrap

--- Codigo PHP/Views/index.php
+++ Codigo PHP/Views/index.php
@@ -6,20 +6,11 @@
 <head>
 <meta charset="utf-8">
 <link rel="stylesheet" type = "text/css" href="../css/styles.css">
-<link rel="stylesheet" href="../Bootstrap/bootstrap-4.3.1-dist/css/bootstrap.min.css">
 <title>Tienda</title>
-
 </head>
 <body>
-<h1 id="T_principal">Ventas</h1>
-<form >
-    <div class="container">
-        <p>texto de ejemplo</p>
-    </div>
-
-</form>
+    
+        <h1 class="T_principal">ahora</h1>
+   
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="../Bootstrap/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
 </html>
