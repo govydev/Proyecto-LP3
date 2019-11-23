b78396c142a3e9c2ff675fdd2b625391ec382c4c
flavio <favio789@gmail.com>
2019-11-23 11:14:28 -0500

    se mejoro presentación de cajas de texto

--- Codigo PHP/Vista/form_distribuidor.php
+++ Codigo PHP/Vista/form_distribuidor.php
@@ -41,25 +41,23 @@ $elemento = distribuidorDAO::buscarId($_GET['id']);
   </div>
 </nav>
 
-  <center>
-    <form action="../Controlador/distribuidorControlador.php?a=modificar" method="POST">
-        <div class="form-group">
-          <input type="hidden" name="id" value = "<?= $elemento[0]?>">
-        </div>
-        <div class="form-group">
-          <label >Nombre: </label><br>
-          <input type="text" class="form-control-6" placeholder="Ingrese Nombre" name="nombre" value = "<?= $elemento[1]?>" required>
-        </div>
-        <div class="form-group">
-          <label >RUC: </label><br>
-          <input type="text" class="form-control-6" placeholder="Ingrese RUC" name="ruc" value = "<?= $elemento[2]?>" required>
-        </div>
-        
-        <button type="submit" class="btn btn-outline-dark">Actualizar</button>
-    </form>
-  </center>
+<center>
+  <form action="../Controlador/distribuidorControlador.php?a=modificar" method="POST">
+      <div class="col-2">
+        <input type="hidden" name="id" value = "<?= $elemento[0]?>">
+      </div>
+      <div class="col-2">
+        <label >Nombre: </label><br>
+        <input type="text" class="form-control" placeholder="Ingrese Nombre" name="nombre" value = "<?= $elemento[1]?>" required>
+      </div>
+      <div class="col-2">
+        <label >RUC: </label><br>
+        <input type="text" class="form-control" placeholder="Ingrese RUC" name="ruc" value = "<?= $elemento[2]?>" required>
+      </div>
+      
+      <br><button type="submit" class="btn btn-outline-dark">Actualizar</button>
+  </form>
+</center>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script> 
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </html>
--- Codigo PHP/Vista/form_producto.php
+++ Codigo PHP/Vista/form_producto.php
@@ -39,30 +39,28 @@ $elemento = productoDAO::buscarId($_GET['id']);
 
 <center>
   <form action="../Controlador/productoControlador.php?a=modificar" method="POST">
-    <div class="form-group">
+    <div class="col-2">
       <input type="hidden" name="id" value = "<?= $elemento[0]?>">
     </div>
-    <div class="form-group">
+    <div class="col-2">
       <label >Nombre</label>
       <input type="text" class="form-control" placeholder="Ingrese Nombre" name="nombre" value = "<?= $elemento[1]?>" required>
     </div>
-    <div class="form-group">
+    <div class="col-2">
       <label >Stock</label>
       <input type="text" class="form-control" placeholder="Ingrese Stock" name="stock" value = "<?= $elemento[2]?>" required>
     </div>
-    <div class="form-group">
+    <div class="col-2">
       <label >Fecha de Vencimiento</label>
       <input type="date" class="form-control" placeholder="Ingrese Fecha de Vencimiento" name="fecha_vencimiento" value = "<?= $elemento[3]?>" required>
     </div>
-    <div class="form-group">
+    <div class="col-2">
       <label >Precio de Venta</label>
       <input type="text" class="form-control" placeholder="Ingrese Precio de Venta" name="precio_venta" value = "<?= $elemento[4]?>" required>
     </div>
-    <button type="submit" class="btn btn-outline-dark">Actualizar</button>
+    <br><button type="submit" class="btn btn-outline-dark">Actualizar</button>
   </form>
 </center>
-</body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
