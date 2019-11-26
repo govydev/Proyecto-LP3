de74500b410e997223ca4c4b9ce8f60f56767339
flavio <favio789@gmail.com>
2019-11-26 11:24:48 -0500

    restablecimiento de archivo config.php

---
+++ Codigo PHP/Conexion/config.php
@@ -0,0 +1,8 @@
+<?php
+//configurar los parametros para la conexion a la base de datos
+
+DEFINE("DB_HOST","localhost");
+DEFINE("DB_USUARIO","root");
+DEFINE("DB_CONTRA","");
+DEFINE("DB_NOMBRE","bodega");
+DEFINE("DB_CHARSET", "utf8");
