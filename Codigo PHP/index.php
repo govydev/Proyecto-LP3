c0169524fdfa02d9915f3341a21e549e9e70fbe9
flavio <favio789@gmail.com>
2019-11-28 13:16:30 -0500

    validacion de ingreso en proceso

--- Codigo PHP/index.php
+++ Codigo PHP/index.php
@@ -7,10 +7,23 @@
     <link rel="stylesheet" type = "text/css" href="Estilo/css/style_index.css">
     <link rel="stylesheet" href="Estilo/alertifyjs/css/alertify.min.css" />
     <link rel="stylesheet" href="Estilo/alertifyjs/css/themes/default.min.css" />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'><link rel="stylesheet" href="./style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <title>Goliat</title>
 </head>
 <body>
+<nav class="sidebar-navigation">
+	<ul>
+		<li class="active">
+        <i class="fa fa-sliders"></i>
+			<span class="tooltip">Settings</span>
+		</li>
+	</ul>
+</nav>
+    <div class="centrar">
     <a class="titulo" onclick="validar()">Bienvenido a Goliat</a>
+    </div>
+    
     <script src="Estilo/alertifyjs/alertify.js"></script>
 </body>
 </html>
@@ -29,4 +42,11 @@
         ); 
         
     }
+</script>
+
+<script type="text/javascript">
+	$('ul li').on('click', function() {
+	$('li').removeClass('active');
+	$(this).addClass('active');
+});
 </script>
